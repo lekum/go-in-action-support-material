791c7a095310c674cd40ba8b5c21528226ed4cf9
Alejandro Guirao <lekumberri@gmail.com>
2016-11-14 08:20:19 +0100

    Add example of copying arrays

--- chapter_4/arrays/basic.go
+++ chapter_4/arrays/basic.go
@@ -19,4 +19,26 @@ func main() {
 	printSlice(a)
 	b := [...]int{10, 20, 30, 40, 50}
 	printArray(b)
+	var c [5]int
+	c = b
+	c[0] = 1
+
+	fmt.Println("b after c changes:")
+	printArray(b)
+
+	fmt.Println("c after change:")
+	printArray(c)
+
+	ar := [...]*string{new(string), new(string), new(string)}
+	*ar[0] = "Red"
+	*ar[1] = "Blue"
+	*ar[2] = "Green"
+	ab := ar
+	*ar[2] = "Yellow"
+	for _, x := range ar {
+		fmt.Println(*x)
+	}
+	for _, x := range ab {
+		fmt.Println(*x)
+	}
 }
