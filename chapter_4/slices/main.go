f93b9275212a0d0be03a35382f5a587dc1f52990
Alejandro Guirao <lekumberri@gmail.com>
2016-11-17 09:35:22 +0100

    Add empty and nil slices

--- chapter_4/slices/main.go
+++ chapter_4/slices/main.go
@@ -6,4 +6,7 @@ func main() {
 	slice1 := make([]string, 5)    // length and capacity of 5 elements
 	slice2 := make([]string, 3, 5) // length of 3 and capacity of 5 elements
 	fmt.Println(slice1, slice2)
+	empty_slice := make([]int, 0)
+	var nil_slice []int
+	fmt.Println(empty_slice, nil_slice)
 }
