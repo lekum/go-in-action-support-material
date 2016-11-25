3f06ef035e17daf5c147a2ad0726a958da4be6af
Alejandro Guirao <lekumberri@gmail.com>
2016-11-25 08:32:39 +0100

    Add multidimensional slices

--- chapter_4/slices/main2.go
+++ chapter_4/slices/main2.go
@@ -8,4 +8,8 @@ func main() {
 		fmt.Printf("Value: %d Value-Addr: %X ElemAddr: %X\n",
 			value, &value, &slice[index])
 	}
+	slice2 := [][]int{{10}, {100, 200}}
+	fmt.Println(slice2)
+	slice2[0] = append(slice2[0], 20)
+	fmt.Println(slice2)
 }
