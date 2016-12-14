6703e4f7021256d58cc3aabf00655394ea54959f
Alejandro Guirao <lekumberri@gmail.com>
2016-12-14 09:36:50 +0100

    Add type embedding example

---
+++ chapter_5/embedding/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type notifier interface {
+	notify()
+}
+
+func sendNotification(n notifier) {
+	n.notify()
+}
+
+type user struct {
+	name  string
+	email string
+}
+
+func (u *user) notify() {
+	fmt.Printf("Sending user email to %s<%s>\n",
+		u.name,
+		u.email)
+}
+
+type admin struct {
+	user  // Embedded type
+	level string
+}
+
+func main() {
+	ad := admin{
+		user: user{
+			name:  "John Smith",
+			email: "john@email.com",
+		},
+		level: "super",
+	}
+	sendNotification(&ad)
+}
