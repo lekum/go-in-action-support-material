ecc00b5bb8db7e74776b356c76bdc540c5f0f200
Alejandro Guirao <lekumberri@gmail.com>
2016-12-28 10:02:38 +0100

    Add example of atomic.StoreInt64

---
+++ chapter_6/atomic_functions/main2.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	// shutdown is a flag to alert running goroutines to shutdown
+	shutdown int64
+	wg       sync.WaitGroup
+)
+
+func main() {
+	wg.Add(2)
+	go doWork("A")
+	go doWork("B")
+
+	// Give the goroutines time to run
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("Shutdown now")
+	atomic.StoreInt64(&shutdown, 1)
+
+	wg.Wait()
+}
+
+func doWork(id string) {
+	defer wg.Done()
+	for {
+		fmt.Printf("Doing %s Work\n", id)
+		time.Sleep(250 * time.Millisecond)
+		if atomic.LoadInt64(&shutdown) == 1 {
+			fmt.Printf("Shutting %s Down\n", id)
+			break
+		}
+	}
+}
