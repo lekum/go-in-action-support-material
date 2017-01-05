9f934ac0e04ec1d9709efba9abda0c5d95b9d29b
Alejandro Guirao <lekumberri@gmail.com>
2017-01-05 10:30:24 +0100

    Add example of using the connection pool

---
+++ chapter_7/pool_examples/example1.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/lekum/go-in-action-support-material/chapter_7/pool"
+)
+
+const (
+	maxGoroutines   = 25
+	pooledResources = 2
+)
+
+// dbConnection simulates a resource to share
+type dbConnection struct {
+	ID int32
+}
+
+// Close implements the io.Closer interface so dbConnection can
+// be managed by the pool. Close performs any resource release management
+func (dbConn *dbConnection) Close() error {
+	log.Println("Close: Connection", dbConn.ID)
+	return nil
+}
+
+var idCounter int32
+
+func createConnection() (io.Closer, error) {
+	id := atomic.AddInt32(&idCounter, 1)
+	log.Println("Create: New Connection", id)
+	return &dbConnection{ID: id}, nil
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(maxGoroutines)
+
+	p, err := pool.New(createConnection, pooledResources)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for query := 0; query < maxGoroutines; query++ {
+		go func(q int) {
+			performQueries(q, p)
+			wg.Done()
+		}(query)
+	}
+
+	wg.Wait()
+	log.Println("Shutdown Program.")
+	p.Close()
+
+}
+
+func performQueries(query int, p *pool.Pool) {
+	conn, err := p.Acquire()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer p.Release(conn)
+
+	// Wait to simulate a query response
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	log.Printf("QID[%d] CID[%d]\n", query, conn.(*dbConnection).ID)
+}
