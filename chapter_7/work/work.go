0526c59e3079c767f45e3511d040d82bdd3d74e0
Alejandro Guirao <lekumberri@gmail.com>
2017-01-09 09:28:19 +0100

    Add example of a goroutine pool

---
+++ chapter_7/work/work.go
@@ -0,0 +1,40 @@
+// Package work manages a pool of goroutines to perform work
+package work
+
+import "sync"
+
+// Worker must be implemented by types that want to use the work pool.
+type Worker interface {
+	Task()
+}
+
+// Pool provides a pool of goroutines that can execute any Worker tasks that are submitted.
+type Pool struct {
+	work chan Worker
+	wg   sync.WaitGroup
+}
+
+func New(maxGoroutines int) *Pool {
+	p := Pool{
+		work: make(chan Worker),
+	}
+	p.wg.Add(maxGoroutines)
+	for i := 0; i < maxGoroutines; i++ {
+		go func() {
+			for w := range p.work {
+				w.Task()
+			}
+			p.wg.Done()
+		}()
+	}
+	return &p
+}
+
+func (p *Pool) Run(w Worker) {
+	p.work <- w
+}
+
+func (p *Pool) Shutdown() {
+	close(p.work)
+	p.wg.Wait()
+}
