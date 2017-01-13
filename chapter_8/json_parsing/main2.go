b2b7092db874edfba1ce0ab7f7bfe957e05d7e0c
Alejandro Guirao <lekumberri@gmail.com>
2017-01-13 11:16:19 +0100

    Add example of JSON unmarshalling from string

---
+++ chapter_8/json_parsing/main2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Contact struct {
+	Name    string `json:"name"`
+	Title   string `json:"title"`
+	Contact struct {
+		Home string `json:"home"`
+		Cell string `json:"cell"`
+	} `json:"contact"`
+}
+
+var JSON = `{
+	"name": "Gopher",
+	"title": "programmer",
+	"contact": {
+		"home": "415.333.3333",
+		"cell": "415.555.5555"
+	}
+}`
+
+func main() {
+	var cnt Contact
+	err := json.Unmarshal([]byte(JSON), &cnt)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+	fmt.Println(cnt)
+}
