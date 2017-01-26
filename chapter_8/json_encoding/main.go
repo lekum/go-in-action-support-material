10748903115fee6d10078301d2b9a5547adde0b0
Alejandro Guirao <lekumberri@gmail.com>
2017-01-26 09:30:25 +0100

    Add example of json.MarshalIndent

---
+++ chapter_8/json_encoding/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func main() {
+	c := make(map[string]interface{})
+	c["name"] = "Gopher"
+	c["title"] = "programmer"
+	c["contact"] = map[string]interface{}{
+		"home": "415.333.3333",
+		"cell": "415.555.5555",
+	}
+	data, err := json.MarshalIndent(c, "", "    ")
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+	fmt.Println(string(data))
+}
